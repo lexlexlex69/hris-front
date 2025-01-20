0893e8141bca659a966d2bb20cbdf4bb98ab910c
lexlexlex69 <hunilecks@gmail.com>
2025-01-20 12:13:52 +0800

    asdfdsafsdfsdfsdf

--- src/pages/layout/prf/printables/components/CustomJOinput.js
+++ src/pages/layout/prf/printables/components/CustomJOinput.js
@@ -38,7 +38,6 @@ function CustomJOinput({ title, objectName }) {
   const [inputState, setInputState] = useState("");
   const [objectState, setObjectState] = useState();
   const [open, setOpen] = useState();
-  const matches = useMediaQuery("(min-width: 565px)");
 
   useEffect(() => {
     console.log("prfDatajo", prfData);
@@ -91,21 +90,7 @@ function CustomJOinput({ title, objectName }) {
         >
           open modsal
         </button>
-        <CustomCenterModal
-          key={"open1"}
-          matches={matches}
-          openner={open}
-          comptitle={"asdfsadf"}
-          compSize={"40%"}
-          handleCloseBTN={() => setOpen(false)}
-        >
-          <TextField
-            id="outlined-basic"
-            label="Outlined"
-            variant="outlined"
-            sx={{ width: "100%" }}
-          />
-        </CustomCenterModal>
+
         <button onClick={handleAddItem}>Add</button>
         <ol>
           {objectState &&
@@ -142,5 +127,132 @@ function CustomJOinput({ title, objectName }) {
     </CustomInputTemplate>
   );
 }
+
+const CustomModalJo = ({ openner, comptitle, handleCloseBTN }) => {
+  const matches = useMediaQuery("(min-width: 565px)");
+
+  const {
+    closeModal,
+    handleRowClick,
+    fetchEmployeeByName,
+    employeeList,
+    handleModalRowClick,
+    modalTitle,
+  } = usePrfData();
+
+  const [name, setName] = useState();
+  const [tempData, setTempData] = useState({});
+  tempData && console.log("viewlist tempdata", tempData);
+
+  return (
+    <CustomCenterModal
+      key={"open1"}
+      matches={matches}
+      openner={openner}
+      comptitle={comptitle}
+      compSize={"40%"}
+      handleCloseBTN={handleCloseBTN}
+    >
+      <Box sx={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+        <Box
+          sx={{ display: "flex", justifyContent: "space-between", gap: "15px" }}
+        >
+          <TextField
+            id="outlined-basic"
+            label={"Employee Name (first name / last name)"}
+            variant="outlined"
+            sx={{ width: "100%" }}
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+
+          <Button variant="contained" onClick={() => fetchEmployeeByName(name)}>
+            Search
+          </Button>
+        </Box>
+        <Box>
+          <Stack
+            direction="column"
+            // justifyContent="center"
+            alignItems="center"
+            spacing={1}
+            overflow={"auto"}
+            sx={{ maxHeight: "50vh", marginTop: "10px" }}
+          >
+            {employeeList &&
+              employeeList.map((item, index) => (
+                <Item
+                  key={index}
+                  sx={{
+                    // display: "flex",
+                    // flexDirection: "column",
+                    cursor: "pointer",
+                    backgroundColor:
+                      tempData.id == item.id ? "#f2f2f2" : "none",
+                    width: "100%",
+                    textAlign: "left",
+                    fontSize: "1rem",
+                  }}
+                  onClick={() => setTempData(item)}
+                >
+                  <Typography sx={{ fontWeight: "600" }}>
+                    {item.fname} {!item.mname && !item.mname} {item.lname}{" "}
+                    {!item.extname && item.extname}
+                  </Typography>
+
+                  <Typography>{item.position_name}</Typography>
+
+                  <Typography>{item.dept_title}</Typography>
+                </Item>
+              ))}
+          </Stack>
+          {/* <p
+            key={index}
+            onClick={() => setTempData(item)}
+            style={{
+              backgroundColor:
+                tempData & (tempData.id == item.id) ? "red" : "blue",
+            }}
+          >
+            {item.fname} {!item.mname && !item.mname} {item.lname}{" "}
+            {!item.extname && item.extname} {item.position_name}{" "}
+            {item.dept_title}
+          </p> */}
+        </Box>
+        <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Button variant="text" onClick={handleCloseBTN}>
+            Cancel
+          </Button>
+          <Button
+            variant="contained"
+            onClick={(e) => {
+              e.preventDefault();
+              if (comptitle && comptitle === "PREPARED BY") {
+                handleModalRowClick(
+                  {
+                    prepared_by: `${tempData.fname} ${tempData.mname} ${tempData.lname} ${tempData.extname}`,
+                    prepared_by_position: tempData.position_name,
+                  },
+                  comptitle
+                );
+              } else if (comptitle && comptitle === "ENDORSED BY") {
+                handleModalRowClick(
+                  {
+                    endorsed_by: `${tempData.fname} ${tempData.mname} ${tempData.lname} ${tempData.extname}`,
+                    endorsed_by_position: tempData.position_name,
+                    endorsed_by_department: tempData.dept_title,
+                  },
+                  comptitle
+                );
+              }
+            }}
+          >
+            Save
+          </Button>
+        </Box>
+      </Box>
+    </CustomCenterModal>
+  );
+};
 
 export default CustomJOinput;
